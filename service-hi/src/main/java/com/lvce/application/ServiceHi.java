98ec75ce8994e3ec1b450aaff106ca88ebfcdcc9
zhangzhiqiang <1252320343@qq.com>
2018-10-23 17:23:15 +0800

    第一次提交

---
+++ service-hi/src/main/java/com/lvce/application/ServiceHi.java
@@ -0,0 +1,24 @@
+package com.lvce.application;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient 
+@RestController
+public class ServiceHi {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHi.class, args);
+	}
+	@Value("${server.port}")
+	String port;
+	@RequestMapping("/hi")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name){
+		return "hi "+name+",i am from port:" +port;
+	}
+}
